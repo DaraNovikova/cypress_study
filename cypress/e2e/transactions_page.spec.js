6f87ad5efd10c6451058373146304790a921b673
DaraNovikova <daranvik@gmail.com>
2022-08-09 19:36:38 +0300

    added fourth homework

--- cypress/e2e/transactions_page.spec.js
+++ cypress/e2e/transactions_page.spec.js
@@ -15,6 +15,8 @@ const transaction_description = generateRandomText(5);
 
     beforeEach('visiting sign in page', () => {
       cy.intercept("GET", "/users*").as("usersList");
+      cy.intercept("GET", "/transactions").as("personalTransactions");
+      cy.intercept("PATCH", "/transactions/*").as("updateTransaction");
       cy.visit('/')
     })
   
@@ -62,7 +64,7 @@ const transaction_description = generateRandomText(5);
     .should('be.visible')
     cy.wait('@usersList')
     cy.get(transactions_page.users_search_input)
-    .type('Arely')
+    .type('Edgar')
     cy.get(transactions_page.first_user)
     .click()
     cy.xpath(transactions_page.verifyTransactionStepTitle('Payment'))
@@ -80,8 +82,180 @@ const transaction_description = generateRandomText(5);
     .and("have.text", "Transaction Submitted!");
 })
   // 3. displays new transaction errors
+
+  it('displays new transaction errors', () => {
+    cy.fixture('user_data').its('test_user2').then((user) => {
+    cy.login(user)
+    })
+    cy.get(transactions_page.new_transaction_btn)
+    .click()
+    cy.wait('@usersList')
+    cy.get(transactions_page.users_search_input)
+    .type('Edgar')
+    cy.get(transactions_page.first_user)
+    .click()
+    cy.get(transactions_page.payment_amount_field)
+    .type('10')
+    .clear()
+    .blur();
+    cy.get(transactions_page.amount_required_error)
+    .should("be.visible")
+    .and("contain", "Please enter a valid amount");
+    cy.get(transactions_page.transaction_description_field)
+    .type(transaction_description)
+    .clear()
+    .blur();
+    cy.get(transactions_page.transaction_description_required_error)
+    .should("be.visible")
+    .and("contain", "Please enter a note");
+    cy.get(transactions_page.transaction_pay_btn)
+    .should("be.disabled");
+    cy.get(transactions_page.transaction_request_btn)
+    .should("be.disabled");
+    
+})
+
   // 4. submits a transaction payment and verifies the deposit for the receiver
+
+  it('submits a transaction payment and verifies the deposit for the receiver', () => {
+    cy.fixture('user_data').its('test_user2').then((user) => {
+      cy.login(user)
+    })
+    let startBalance;
+    cy.get("[data-test=sidenav-user-balance]")
+        .invoke("text")
+        .then((x) => {
+          startBalance = +x
+          .replace(',', '')
+          .match(/[\d.]+/g)[0];
+        })
+    cy.logout()
+    cy.fixture('user_data').its('test_user').then((user) => {
+      cy.login(user)
+    })  
+    cy.get(transactions_page.new_transaction_btn)
+    .click()
+    cy.wait('@usersList')
+    cy.get(transactions_page.users_search_input)
+    .type('Arely')
+    cy.get(transactions_page.first_user)
+    .click()
+    cy.get(transactions_page.payment_amount_field)
+    .type('10')
+    cy.get(transactions_page.transaction_description_field)
+    .type(transaction_description)
+    cy.get(transactions_page.transaction_request_btn)
+    .click()
+    cy.get(transactions_page.success_transaction_message)
+    .should("be.visible")
+    .and("have.text", "Transaction Submitted!");
+    cy.logout()
+    cy.fixture('user_data').its('test_user2').then((user) => {
+      cy.login(user)
+    })
+
+    let newBalance;
+    cy.get("[data-test=sidenav-user-balance]")
+        .invoke("text")
+        .then((x) => {
+          newBalance = +x
+          .replace(',', '')
+          .match(/[\d.]+/g)[0];
+          console.log(startBalance)
+          console.log(newBalance)
+          expect(newBalance).to.be.equal(startBalance + 10);
+        })
+})
   // 5. submits a transaction request and accepts the request for the receiver
+
+  it('submits a transaction request and accepts the request for the receiver', () => {
+    cy.fixture('user_data').its('test_user2').then((user) => {
+      cy.login(user)
+    }) 
+    let startBalance;
+    cy.get("[data-test=sidenav-user-balance]")
+        .invoke("text")
+        .then((x) => {
+          startBalance = +x
+          .replace(',', '')
+          .match(/[\d.]+/g)[0];
+    }) 
+    cy.get(transactions_page.new_transaction_btn)
+    .click()
+    cy.wait('@usersList')
+    cy.get(transactions_page.users_search_input)
+    .type('Edgar')
+    cy.get(transactions_page.first_user)
+    .click()
+    cy.get(transactions_page.payment_amount_field)
+    .type('10')
+    cy.get(transactions_page.transaction_description_field)
+    .type(transaction_description)
+    cy.get(transactions_page.transaction_request_btn)
+    .click()
+    cy.get(transactions_page.success_transaction_message)
+    .should("be.visible")
+    .and("have.text", "Transaction Submitted!");
+    cy.logout()
+    cy.fixture('user_data').its('test_user').then((user) => {
+      cy.login(user)
+    })
+
+    cy.get(transactions_page.mine_transaction_tab).click();
+    cy.wait("@personalTransactions");
+    cy.get(transactions_page.mine_transactions)
+    .first()
+    .click({ force: true });
+
+    cy.get(transactions_page.transaction_description_text)
+    .should("contain", transaction_description)
+    cy.get(transactions_page.accept_request_btn)
+    .click();
+    cy.wait("@updateTransaction").its("response.statusCode").should("eq", 204);
+    cy.logout()
+    cy.fixture('user_data').its('test_user2').then((user) => {
+      cy.login(user)
+    })
+    let newBalance;
+    cy.get("[data-test=sidenav-user-balance]")
+        .invoke("text")
+        .then((x) => {
+          newBalance = +x
+          .replace(',', '')
+          .match(/[\d.]+/g)[0];
+          console.log(startBalance)
+          console.log(newBalance)
+          expect(newBalance).to.be.equal(startBalance + 10);
+        })
+})
+
   // 6. searches for a user by attribute
+  it('searches for a user by attribute', () => {
+    const searchUser =  'Edgar Johns';
+    cy.fixture('user_data').its('test_user2').then((user) => {
+    cy.login(user)
+    })
+    cy.get(transactions_page.new_transaction_btn)
+    .click()
+    cy.xpath(transactions_page.verifyTransactionStepTitle('Select Contact'))
+    .should('be.visible')
+    cy.wait('@usersList')
+    cy.fixture('user_data').its('test_user').then((user2) => {
+      cy.get(transactions_page.users_search_input)
+      .type(user2.firstName)
+      cy.xpath(transactions_page.verifyUserIsDisplayed())
+      .should('have.text', searchUser)
+      cy.get(transactions_page.users_search_input).clear()
+      cy.get(transactions_page.users_search_input)
+      .type(user2.lastName)
+      cy.xpath(transactions_page.verifyUserIsDisplayed())
+      .should('have.text', searchUser)
+      cy.get(transactions_page.users_search_input).clear()
+      cy.get(transactions_page.users_search_input)
+      .type(user2.username)
+      cy.xpath(transactions_page.verifyUserIsDisplayed())
+      .should('have.text', searchUser)
+    })
+})
 
   })
--- cypress/selectors/transactions_page.js
+++ cypress/selectors/transactions_page.js
@@ -3,10 +3,17 @@ export const transactions_page = {
     first_user: '[data-test="users-list"] li:first-child',
     users_search_input: '#user-list-search-input',
     payment_amount_field: '#amount',
+    amount_required_error: '#transaction-create-amount-input-helper-text',
     transaction_description_field: '#transaction-create-description-input',
+    transaction_description_required_error: '#transaction-create-description-input-helper-text',
     transaction_request_btn: '[data-test="transaction-create-submit-request"]',
     transaction_pay_btn: '[data-test="transaction-create-submit-payment"]',
     success_transaction_message: '[data-test="alert-bar-success"]',
-
-    verifyTransactionStepTitle(title){ return `//*[text()="${title}"]`}
+    mine_transaction_tab: '[data-test="nav-personal-tab"]',
+    mine_transactions: '[data-test*="transaction-item"]',
+    transaction_description_text: '[data-test="transaction-description"]',
+    accept_request_btn: '[data-test*="transaction-accept-request"]',
+ 
+    verifyTransactionStepTitle(title){ return `//*[text()="${title}"]`},
+    verifyUserIsDisplayed(){ return `//*[@data-test="users-list"]/li[1]/div/span`},
 }
